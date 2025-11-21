6910de75206170f426c0a54bb45099b00ff687b6
Leticia Dias <devv.leticia@gmail.com>
2025-11-20 21:55:29 -0300

    algumas validacoes

--- Fonte/relatorio.py
+++ Fonte/relatorio.py
@@ -2,23 +2,11 @@
 quantidade de vendas por Estado, categoria e status de pedidos'''
 import json
 class Relatorio():
-    def __init__(self, dia, mes, ano, vendas, pagamentos):
-        self.dia = dia
-        self.__mes = mes
-        self.__ano = ano
+    def __init__(self, data_relatorio, vendas, pagamentos):
+        self.data_relatorio = data_relatorio
         self.vendas = vendas
         self.__pagamentos = pagamentos
         
-        @property
-        def dia(self):
-            return self.__dia
-        @dia.setter
-        def dia(self, dia_valido):
-            if len(dia_valido) == 2:
-                dia_valido = self.__dia
-            else:
-                print("O dia deve coonter apenas dois dígitos")
-
     def faturamento_periodo(self):
         pass
     def ranking(self):
